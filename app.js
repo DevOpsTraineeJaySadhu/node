9ed4bd284b677d4deebe1009df004f516b285c57
DevOpsTraineeJaySadhu <jay.sadhu@codezeros.com>
2024-11-20 18:00:03 +0530

    New work

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
+// app.js
+import http from 'http';
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Wodld!\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
